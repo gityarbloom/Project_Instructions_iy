6d53628ec4c7766ab0cf437e3622a52f2c753980
Israel-Yarbloom <i0504170922@gmail.com>
2025-12-23 20:21:44 +0200

    add a basic fastapi server

--- app/main.py
+++ app/main.py
@@ -3,4 +3,14 @@ import uvicorn
 
 app = FastAPI()
 
-@app.get('/')
+@app.get("/")
+def say_hi():
+    return "yaaaaaaay!!"
+
+@app.get("/contacts")
+def get_all_contacts():
+    return "all contacts"
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
