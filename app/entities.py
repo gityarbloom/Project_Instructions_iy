3a36084b22cbd2f2c46b78a70a5cf05f76a5f193
Israel-Yarbloom <i0504170922@gmail.com>
2025-12-23 16:08:30 +0200

    Initial project structure

--- app/entities.py
+++ app/entities.py
@@ -1,6 +1,5 @@
 class Contact:
-    def __init__(self, id:int , first_name:str, last_name:str, phone_number:str):
-        self.id = id
+    def __init__(self, id:int=None , first_name:str, last_name:str, phone_number:str):
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
