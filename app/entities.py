e86c47b08b26c25c39a34e5c90b46e2d530baba4
Israel-Yarbloom <i0504170922@gmail.com>
2025-12-23 15:45:23 +0200

    Initial project structure

---
+++ app/entities.py
@@ -0,0 +1,14 @@
+class Contact:
+    def __init__(self, id:int , first_name:str, last_name:str, phone_number:str):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+
+    def to_dict(self):
+        self = self.__dict__
+        return self
+
+# cont = Contact("211846894", "israel", "yarbloom", "053-5930755")
+# print(cont.to_dict())
+
