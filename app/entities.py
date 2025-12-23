d025108b47c0dd373602eb074c8be00e10fa52eb
Israel-Yarbloom <i0504170922@gmail.com>
2025-12-23 16:13:19 +0200

    Add database schema and init script

--- app/entities.py
+++ app/entities.py
@@ -1,5 +1,5 @@
 class Contact:
-    def __init__(self, id:int=None , first_name:str, last_name:str, phone_number:str):
+    def __init__(self, id:int|None, first_name:str, last_name:str, phone_number:str):
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
@@ -7,7 +7,3 @@ class Contact:
     def to_dict(self):
         self = self.__dict__
         return self
-
-# cont = Contact("211846894", "israel", "yarbloom", "053-5930755")
-# print(cont.to_dict())
-
